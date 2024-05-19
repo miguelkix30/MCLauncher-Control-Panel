a6ebd786d0051dafeb3f066cc8907cc475811fe5
Miguelki <63244249+miguelkix30@users.noreply.github.com>
2024-05-19 21:53:24 +0200

    Add a very simple text editor to clients

--- ceditor.php
+++ ceditor.php
@@ -6,7 +6,17 @@ if (!isset($_SESSION["username"])) {
     header("Location: index.php");
     exit;
 }
+
+$file = '../files/php/instances.php';
+if (isset($_POST['text'])) {
+    file_put_contents($file, $_POST['text']);
+    echo "Archivo guardado.";
+}
+$text = file_get_contents($file);
+
 ?>
+
+
 <head>
     <title>MC Launcher Admin Panel</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
@@ -17,12 +27,12 @@ if (!isset($_SESSION["username"])) {
     <nav>
         <a href="config.php">Configuración del launcher</a>
         <a href="bans.php">Administrar bloqueos de HWID</a>
-        <a href="instances.php">Clientes</a>
+        <a href="ceditor.php">Clientes</a>
         <a href="logout.php">Cerrar sesión</a>
     </nav><br><br>
+    <form class="editor-form" action="" method="post">
+    <textarea class="editor-content" name="text" rows="30" cols="80"><?php echo htmlspecialchars($text) ?></textarea><br>
+    <input class="editor-submit" type="submit" />
+</form>
 
-    <body>
-    <h1>Configurador de Clientes</h1>
-    <h2>TODO: hacer el Configurador de Clientes</h>
-</body>
 </body>
